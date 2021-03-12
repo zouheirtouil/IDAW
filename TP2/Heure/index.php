3839817708251ef02a2749083bbc64ac244cd184
unknown <z.touil@enim.ac.ma>
2021-03-12 09:55:24 +0100

    Code Php pour avoir le jour et l'heure

---
+++ TP2/Heure/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+ <html>
+     <head>
+         <title>L'heure</title>
+         <link charset="utf-8" />
+    </head>
+
+    <body>
+
+        <h1>Voici Le Jour: </body>
+        <?php  echo date('Y-m-d'); ?>
+        <h1>Voici L'heure courante: </body>
+        <?php  echo date(' H:i:s'); ?>
+ 
+
+</html>
