90ac5718d485c7e3a9600b5160d6b53e136d24bb
unknown <z.touil@enim.ac.ma>
2021-03-12 10:11:01 +0100

    création d’un tableau et écrire une fonction

--- TP2/Heure/index.php
+++ TP2/Heure/index.php
@@ -7,10 +7,32 @@
 
     <body>
 
-        <h1>Voici Le Jour: </body>
-        <?php  echo date('Y-m-d'); ?>
-        <h1>Voici L'heure courante: </body>
-        <?php  echo date(' H:i:s'); ?>
- 
+        <h1>Voici Le Jour: 
+        <?php  echo date('Y-m-d'); ?>  </h1>
+        <h1>Voici L'heure courante: 
+        <?php  echo date(' H:i:s'); ?>   </h1>
+        
+    
+       <?php
+        function afficher( $texte, $saut = 1 ) {
+            echo $texte;
+            for( $i = 0 ; $i < $saut ; $i++)
+            echo "\n";
+            }
+            afficher("Hello", 2);
+       
+        $personne = array(
+            'prenom' => 'Zouheir',
+            'nom' => 'TOUIL',
+            'age' => 22
+            );
+
+            afficher("M. ".$personne['prenom'], 2);
+            afficher( "M. {$personne['nom']}\n", 2);
+
+        ?>
+    
+    
+    </body>
 
 </html>
