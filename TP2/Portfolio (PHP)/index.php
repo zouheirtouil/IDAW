d2b2464faa9480023cb6cea01885b600398b817f
unknown <z.touil@enim.ac.ma>
2021-03-12 11:01:26 +0100

    modification du fichier index.php en utilisant template_menu.php

--- TP2/Portfolio (PHP)/index.php
+++ TP2/Portfolio (PHP)/index.php
@@ -1,31 +1,24 @@
-<!DOCTYPE html>
-<html>
-    <?php
+ <?php
+    require_once('template_header.php');
+?>
+
+<header>
+    <h1>TOUIL Zouheir :  ETUDIANT DOUBLE DIPLOME IMT LILLE DOUAI</h1>
+</header>
+
+<?php
         require_once('template_header.php');
-    ?>
-  <body>
-      <header>
-          <h1>TOUIL Zouheir</h1>
-    </header>
+?>
+    
+<img src="Images/photo.PNG" alt="Mon image">
 
-
-    <nav class="menu">
-        <ul>
-        <li><a href="index.php">Accueil</a></li>
-        <li><a href="cv.php">CV</a></li>
-        <li><a href="projets.php">Projets</a></li>
-        </ul>
-        </nav>
-        
-    <img src="Images/photo.PNG" alt="Mon image">
-
-    <article> 
-        <h2>Qui suis-je?</h2>
+<article> 
+    <h2>Qui suis-je?</h2>
     <p> Etudiant ingénieur en double diplômes, diplômé dans les domaines du Génie Logiciel (Software Engineering) et Management des Systèmes de l'Information de l'Ecole Nationale Supérieure des Mines Rabat-Maroc, et actuellement, je suis étudiant en (M1) à l'IMT LILLE DOUAI.</p>
-    </article>
+</article>
 
-    <?php
+
+<?php
         require_once('template_footer.php');
-    ?>
-</body>
-</html>
+ ?>
+
---
+++ TP2/Portfolio (PHP)/template_menu.php
@@ -0,0 +1,11 @@
+<!doctype html>
+    <html>
+        nav class="menu">
+        <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="cv.php">Cv</a></li>
+        <li><a href="projets.php">Mes Projets</a></li>
+        </ul>
+        </nav>
+
+    <html>
