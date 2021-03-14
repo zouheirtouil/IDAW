bcb36b20bb801c825d9ac1c1f34f5c55988edc82
unknown <z.touil@enim.ac.ma>
2021-03-14 11:03:30 +0100

    completer la fonction renderMenuToHTML()

--- TP2/Portfolio (PHP)/template_menu.php
+++ TP2/Portfolio (PHP)/template_menu.php
@@ -1,11 +1,28 @@
 <?php
         function renderMenuToHTML($currentPageId){
-            echo "<nav class=\"menu\">
+           /* echo "<nav class=\"menu\">
             <ul>
             <li><a href=\"index.php\">Accueil</a></li>
             <li><a href=\"cv.php\">Cv</a></li>
             <li><a href=\"projets.php\">Mes Projets</a></li>
             </ul>
             </nav>";
-        }   
+
+            */
+
+
+            // un tableau qui d\'efinit la structure du site
+            $mymenu = array(
+                // idPage titre
+                'index' => array( 'Accueil' ),
+                'cv' => array( 'Cv' ),
+                'projets' => array('Mes Projets')
+            );
+            echo "<nav class=\"menu\"><ul>";
+            foreach($mymenu as $pageId => $pageParameters) {
+                echo "<li><a href='$pageId.php'><span>$pageParameters[0]</span></a></li>";
+                }
+            echo  "</ul></nav>";
+
+     }
 ?>
