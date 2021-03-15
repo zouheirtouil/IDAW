eeaf33482f5793835237b6a425d69abe09f8c1ff
unknown <z.touil@enim.ac.ma>
2021-03-15 11:49:43 +0100

    Partie 7

--- TP2/Portfolio (PHP)/template_menu.php
+++ TP2/Portfolio (PHP)/template_menu.php
@@ -8,26 +8,20 @@
             $lang = "fr";
             $mymenu = array(
                 // idPage titre
-                'accueil' => array( 'Accueil' ),
-                'cv' => array( 'Cv' ),
-                'projets' => array('Mes Projets'),
-                'contact' => array('Contacts'),
-                'lang' => array('English')
+                'accueilfr' => array( 'Accueil' ),
+                'cvfr' => array( 'Cv' ),
+                'projetsfr' => array('Mes Projets'),
+                'contactfr' => array('Contacts'),
+                'langfr' => array('English')
             );
             echo "<nav class=\"menu\"><ul>";
             foreach($mymenu as $pageId => $pageParameters) {
                 echo "<li><a ";
-                if($lang == "fr"){
                 if($currentPageId == $pageId)
-                    if($currentPageId == 'lang'){
-                        $lang = "en";
-                        $currentPageId = 'accueil';
-                    }
-                echo "id=\"currantpage\"";  
-                echo "href=index.php?page=$pageId$lang>$pageParameters[0]</a></li>";
+                    echo "id=\"currantpage\"";  
+                    echo "href=index.php?page=$pageId>$pageParameters[0]</a></li>";
                 }
-            }
-            echo  "</ul></nav>";
+                echo  "</ul></nav>";
 
      }
 ?>
