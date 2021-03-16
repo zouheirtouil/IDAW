dc9641d8ae8bc3ac7a950af58eaba26744f87f9a
unknown <z.touil@enim.ac.ma>
2021-03-16 19:53:09 +0100

    TP3 modification du centenu du fichier connected.php

--- TP3/connected.php
+++ TP3/connected.php
@@ -1,6 +1,27 @@
+
 <?php
-echo "Voici le login :";
-echo $_GET['login'];
-echo '\n Voici le mots de passe :      ' ; 
-echo $_GET['password'];
+    // on simule une base de données
+    $users = array(
+    // login => password
+    'riri' => 'fifi',
+    'yoda' => 'maitrejedi' );
+    $login = "anonymous";
+    $errorText = "";
+    $successfullyLogged = false;
+    if(isset($_GET['login']) && isset($_GET['password'])) {
+        $tryLogin=$_GET['login'];
+        $tryPwd=$_GET['password'];
+    // si login existe et password correspond
+    if( array_key_exists($tryLogin,$users) && $users[$tryLogin]==$tryPwd ) {
+        $successfullyLogged = true;
+        $login = $tryLogin;
+    } else
+        $errorText = "Erreur de login/password";
+    } else
+        $errorText = "Merci d'utiliser le formulaire de login";
+    if(!$successfullyLogged) {
+        echo $errorText;
+    } else {
+        echo "<h1>Bienvenu ".$login."</h1>";
+    }
 ?>
