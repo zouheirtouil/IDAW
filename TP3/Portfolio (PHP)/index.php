f934becf1ce871410ed5477432ea895041fe9ff2
unknown <z.touil@enim.ac.ma>
2021-03-17 22:51:48 +0100

    partie 3

---
+++ TP3/Portfolio (PHP)/index.php
@@ -0,0 +1,32 @@
+<?php
+    require_once("template_header.php");
+    require_once("template_menu.php");
+
+    $currentPageId = 'accueil';
+    if(isset($_GET['page'])) {
+        $currentPageId = $_GET['page'];
+    }
+        
+    if(isset($_GET['lang']) && !empty($_GET['lang']))
+            $lang = $_GET['lang'];
+        
+?>
+
+
+    
+
+
+<section class="corps">
+    <?php
+    $pageToInclude = $currentPageId . ".php";
+    if(is_readable($pageToInclude))
+        require_once($pageToInclude);
+    else
+        require_once("error.php");
+    ?>
+</section>
+
+<?php
+        require_once('template_footer.php');
+ ?>
+
