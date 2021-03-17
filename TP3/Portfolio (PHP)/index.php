9d7decb539ecfd6d0c8060268cb4231e8b24a70f
unknown <z.touil@enim.ac.ma>
2021-03-17 22:52:28 +0100

    partie 3

--- TP3/Portfolio (PHP)/index.php
+++ TP3/Portfolio (PHP)/index.php
@@ -13,6 +13,19 @@
 ?>
 
 
+
+<form id="style_form" action="index.php" method="GET">
+<select name="css">
+<option value="style1">style1</option>
+<option value="style2">style2</option>
+</select>
+<input type="submit" value="Appliquer" />
+</form>
+
+
+<?php
+    renderMenuToHTML($currentPageId);
+?>
     
 
 
