3981dafc8f15dcc42867cf1435dcaccd27981be5
unknown <z.touil@enim.ac.ma>
2021-03-20 21:10:06 +0100

    TP3 PARTIE POST terminer

--- TP3/index.php
+++ TP3/index.php
@@ -39,22 +39,14 @@
 
     
 
-        <form id="login_form" action="connected.php" method="POST">
-            <table>
-                <tr>
-                <th>Login :</th>
-                    <td><input type="text" name="login"></td>
-                </tr>
-                <tr>
-                    <th>Mot de passe :</th>
-                    <td><input type="password" name="password"></td>
-                </tr>
-                <tr>
-                    <th></th>
-                    <td><input type="submit" value="Se connecter..." /></td>
-                </tr>
-            </table>
-        </form>
+            <form id="style_form" action="index.php" method="GET">
+            <select name="css">
+            <option value="style1">style1</option>
+            <option value="style2">style2</option>
+            </select>
+            <input type="submit" value="Appliquer" />
+            </form>
+
 
     
     
